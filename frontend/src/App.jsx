0b6f06667788c0c8e19d7db5d279ef7057c5ed92
anis <anis@macbook.lan>
2023-08-29 04:47:40 +0200

    change display of shows

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 
-import AddShowModal from './AddShowModal';
-import { useState, useEffect } from 'react';
-import Show from './Show';
+import AddShowModal from "./AddShowModal";
+import { useState, useEffect } from "react";
+import Show from "./Show";
 
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
   const [modalOpened, setModalOpened] = useState(false);
@@ -12,8 +12,7 @@ function App() {
 
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API_PATH}/show`).then((res) => {
-      if (res.status === 200 && Array.isArray(res.data))
-        setShows(res.data);
+      if (res.status === 200 && Array.isArray(res.data)) setShows(res.data);
     });
   }, []);
 
@@ -22,11 +21,30 @@ function App() {
       <h1>Show List</h1>
 
       <button onClick={() => setModalOpened(true)}>Add a show</button>
-      <AddShowModal modalOpened={modalOpened} setModalOpened={(param) => setModalOpened(param)} shows={shows} setShows={setShows} />
+      <AddShowModal
+        modalOpened={modalOpened}
+        setModalOpened={(param) => setModalOpened(param)}
+        shows={shows}
+        setShows={setShows}
+      />
 
-      {shows?.map((show) => <Show key={show._id} id={show._id} name={show.name} overview={show.overview} posterURL={show.posterURL} seasonsWatched={show.seasonsWatched} episodesWatched={show.episodesWatched} shows={shows} setShows={setShows} />)}
+      <div className="shows">
+        {shows?.map((show) => (
+          <Show
+            key={show._id}
+            id={show._id}
+            name={show.name}
+            overview={show.overview}
+            posterURL={show.posterURL}
+            seasonsWatched={show.seasonsWatched}
+            episodesWatched={show.episodesWatched}
+            shows={shows}
+            setShows={setShows}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- frontend/src/Show.jsx
+++ frontend/src/Show.jsx
@@ -1,73 +1,16 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import axios from 'axios';
-import { useState } from 'react';
+// import axios from "axios";
+// import { useState } from "react";
 
-export default function Show({ id, name, overview, posterURL, seasonsWatched, episodesWatched, shows, setShows }) {
-  const [seasons, setSeasons] = useState(seasonsWatched);
-  const [episodes, setEpisodes] = useState(episodesWatched);
-
-  const update = async (id, op, type) => {
-    const data = {};
-
-    if(type === 'SE') {
-      data.seasonsWatched = seasons + (op === 'DEC' ? -1 : 1);
-    } else {
-      data.episodesWatched = episodes + (op === 'DEC' ? -1 : 1);
-    }
-
-    axios.put(`${import.meta.env.VITE_API_PATH}/show/${id}`, data).then((res) => {
-      if(res.status === 200) {
-        if (data.seasonsWatched)
-          setSeasons(data.seasonsWatched);
-        else if (data.episodesWatched)
-          setEpisodes(data.episodesWatched);
-      }
-    });
-  } 
-
+export default function Show({ name, posterURL }) {
   return (
     <div className="show">
-      <div className="show-img">
-        <img src={posterURL} alt="show image" />
-      </div>
-      <div>
-        <h2>{name}</h2>
-        <p className="overview">{overview}</p>
-
-        <div className="show-controls">
-          <div className="show-control">
-            <h3>Seasons watched</h3>
-            <div className="button-group">
-              <button onClick={() => update(id, 'DEC', 'SE')}>-</button>
-              <p>{seasons}</p>
-              <button onClick={() => update(id, 'INC', 'SE')}>+</button>
-            </div>
-          </div>
-
-          <div className="show-control">
-            <h3>Episodes watched</h3>
-            <div className="button-group">
-              <button onClick={() => update(id, 'DEC', 'EP')}>-</button>
-              <p>{episodes}</p>
-              <button onClick={() => update(id, 'INC', 'EP')}>+</button>
-            </div>
-          </div>
-        </div>
-        <button className="deleteShow" onClick={() => {
-          if (window.confirm("are you sure you want to delete this show ?")) {
-            axios.delete(`${import.meta.env.VITE_API_PATH}/show/${id}`).then(res => {
-            if (res.status === 200) {
-              setShows(shows.filter(show => show._id !== id));
-            }
-          });
-          }
-        }}>Delete</button>
-      </div>
-
+      <img src={posterURL} alt="show image" />
+      <h2>{name}</h2>
     </div>
-  )
-} 
+  );
+}
 
 Show.propTypes = {
   id: PropTypes.string.isRequired,
@@ -77,5 +20,5 @@ Show.propTypes = {
   seasonsWatched: PropTypes.number.isRequired,
   episodesWatched: PropTypes.number.isRequired,
   shows: PropTypes.array.isRequired,
-  setShows: PropTypes.func.isRequired
-}
+  setShows: PropTypes.func.isRequired,
+};
